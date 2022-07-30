aa9fe5323e4fdf18691908bf2d3bab9b9cc071d5
KatelevskiyViktor <empirikk@ya.ru>
2022-07-30 12:17:19 +0300

    add diagram FinalControlWoek.jpg

--- Program.cs
+++ Program.cs
@@ -1,38 +1,38 @@
 ﻿
 //first
-// string[] stringsArray = new string[]{"hello", "2", "world", ":-)"}; 
-// string[] stringsArray2 = new string[]{"1234", "1567", "-2", "comp sci"}; 
-// string[] stringsArray3 = new string[]{"Russia", "Denmark", "Kazan"}; 
+string[] stringsArray = new string[]{"hello", "2", "world", ":-)"}; 
+string[] stringsArray2 = new string[]{"1234", "1567", "-2", "comp sci"}; 
+string[] stringsArray3 = new string[]{"Russia", "Denmark", "Kazan"}; 
 
 
-// string[] stringsArrayAfter = new string[4];
-// string[] stringsArrayAfter2 = new string[4];
-// string[] stringsArrayAfter3 = new string[3];
+string[] stringsArrayAfter = new string[4];
+string[] stringsArrayAfter2 = new string[4];
+string[] stringsArrayAfter3 = new string[3];
 
-// void PrintIntArray(string[] array)
-// {
-//     Console.Write("Новый массив: ");
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if(array[i] != null)Console.Write(array[i]  + ", ");
+void PrintIntArray(string[] array)
+{
+    Console.Write("Новый массив: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] != null)Console.Write(array[i]  + ", ");
         
-//     }
-//     Console.WriteLine();
-// }   
+    }
+    Console.WriteLine();
+}   
 
-// for (int i = 0; i < stringsArray.Length; i++)
-// {
-//     if(stringsArray[i].Length < 4)stringsArrayAfter[i] = stringsArray[i];
-//     if(stringsArray2[i].Length < 4)stringsArrayAfter2[i] = stringsArray2[i];
-//     if(i < 3){
-//         if(stringsArray3[i].Length < 4)stringsArrayAfter3[i] = stringsArray3[i];  
-//     }
+for (int i = 0; i < stringsArray.Length; i++)
+{
+    if(stringsArray[i].Length < 4)stringsArrayAfter[i] = stringsArray[i];
+    if(stringsArray2[i].Length < 4)stringsArrayAfter2[i] = stringsArray2[i];
+    if(i < 3){
+        if(stringsArray3[i].Length < 4)stringsArrayAfter3[i] = stringsArray3[i];  
+    }
     
-// }
+}
 
-// PrintIntArray(stringsArrayAfter);
-// PrintIntArray(stringsArrayAfter2);
-// PrintIntArray(stringsArrayAfter3);
+PrintIntArray(stringsArrayAfter);
+PrintIntArray(stringsArrayAfter2);
+PrintIntArray(stringsArrayAfter3);
 //endfirst
 
 
